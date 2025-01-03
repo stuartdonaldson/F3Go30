ba2f14af07140e729bc9d4e21f8b27a2cf2d7ed7
Stuart Donaldson <stu@asyn.com>
2025-01-02 17:15:52 -0800

    updated F30 spreadsheet menu, and added test function

--- script/onOpen.js
+++ script/onOpen.js
@@ -9,74 +9,15 @@ function onOpen()
      .addItem('Initialize Triggers', 'initializeTriggers')
      .addItem('Initialize Sheets', 'initSheets')
      .addItem('Add Next HC Available Notice', 'addNextAvailableNotice')
-     .addItem('test', 'testFunc')
+     .addItem('Run test function', 'testFunction')
      .addToUi(); 
   }
   logActivity('onOpen','');
 }
-function testFunc() 
-{ 
-  userNoticeClear();
-  userNotice("this is a test");
-  userNotice("this is another test");
-  showSidebarFromFile();
+
+// test function invoked by test menu item above.
+function testFunction() 
+{
+  testNotificationSidebar();
+  //  shortenUrlWithBitly('https://docs.google.com/spreadsheets/d/1y2c5r-_R0UJRdjEcDPpQSG9WtepTV6JFuYwCB0xbCak/edit?usp=sharing', '77128118e9be8d7000c153a56edaf733b259e112');
 }
-
-function userNotice(notice) {
-  var ss = SpreadsheetApp.getActiveSpreadsheet();
-  var sheet = ss.getSheetByName('Status');
-  
-  // Create the Status sheet if it doesn't exist
-  if (!sheet) {
-    sheet = ss.insertSheet('Status');
-    Logger.log('Status sheet created.');
-  }
-
-  var cell = sheet.getRange('B2');
-  var currentValue = cell.getValue();
-
-  if (currentValue) {
-    cell.setValue(currentValue + '\n' + notice);
-  } else {
-    cell.setValue(notice);
-  }
-}
-
-function userNoticeClear() {
-  var ss = SpreadsheetApp.getActiveSpreadsheet();
-  var sheet = ss.getSheetByName('Status');
-  
-  // Create the Status sheet if it doesn't exist
-  if (!sheet) {
-    sheet = ss.insertSheet('Status');
-    Logger.log('Status sheet created.');
-  }
-
-  var cell = sheet.getRange('B2');
-  cell.clearContent();
-}
-
-// Function to get messages from the Status sheet
-function getMessages() {
-  var ss = SpreadsheetApp.getActiveSpreadsheet();
-  var sheet = ss.getSheetByName('Status');
-  
-  // Create the Status sheet if it doesn't exist
-  if (!sheet) {
-    sheet = ss.insertSheet('Status');
-    Logger.log('Status sheet created.');
-  }
-  
-  var cellValue = sheet.getRange('B2').getValue();
-  var messages = cellValue.split('\n');
-  return messages;
-}
-
-// Function to show the sidebar
-function showSidebarFromFile() {
-  var html = HtmlService.createHtmlOutputFromFile('Sidebar.html');
-  SpreadsheetApp.getUi().showSidebar(html);
-}
-
-
-
