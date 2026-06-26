56f0ca95116bd9c9b69b18b9816faba93993e731
Stuart Donaldson <stuart.donaldson@gmail.com>
2026-06-26 15:12:35 -0700

    bump patch version

--- script/version.js
+++ script/version.js
@@ -1,6 +1,6 @@
 // F3 Go30 Tracker — version info
-const APP_VERSION      = '2.2.44';
-const APP_VERSION_DATE = '2026-06-26T05:39:30.047Z';
+const APP_VERSION      = '2.2.46';
+const APP_VERSION_DATE = '2026-06-26T21:48:51.654Z';
 const APP_AUTHOR       = 'Stuart Donaldson (F3 Little John)';
 const APP_CONTACT      = 'stu@asyn.com';
 const APP_DEPLOY_TARGET  = 'TEMPLATE';
